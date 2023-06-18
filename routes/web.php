b55f93258bfd390de45e13980b95422f5bbcdda4
SakiToshima <saki.02070@icloud.com>
2023-06-18 10:29:55 +0900

    変更内容を記述するメッセージ

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController; 
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,5 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () { return view('welcome'); });
-Route::get('/posts', [PostController::class, 'index']);   
+Route::get('/', [PostController::class, 'index']);
+  
