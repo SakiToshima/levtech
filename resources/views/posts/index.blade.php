62ad238c2685d80cd29708ae6218d47198ff3461
SakiToshima <saki.02070@icloud.com>
2023-06-19 09:54:12 +0900

    add 記事作成画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,7 @@
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class="posts">
             @foreach($post as $post)
                 <div class="post">
