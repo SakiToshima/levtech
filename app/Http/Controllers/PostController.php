90ebde523b48d11ee58a56e4c546ada7efe765a1
SakiToshima <saki.02070@icloud.com>
2023-06-22 12:12:08 +0900

    add ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -20,4 +21,10 @@ class PostController extends Controller
      {
             return view('posts.create');
      }
+        public function store(PostRequest $request, Post $post)
+        {
+            $input = $request['post'];
+            $post->fill($input)->save();
+            return redirect('/posts/' . $post->id);
+        }
 }
